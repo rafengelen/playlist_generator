fbf867fd8ca83886a618b507bba3028bafd0087c
rafengelen <rafengelen01@gmail.com>
2023-12-28 18:07:18 +0100

    r

--- songlibrary-service/src/main/java/fact/it/songlibraryservice/service/SongService.java
+++ songlibrary-service/src/main/java/fact/it/songlibraryservice/service/SongService.java
@@ -48,19 +48,62 @@ public class SongService {
             song3.setCode(UUID.randomUUID().toString());
 
             Song song4 = new Song();
-            song4.setTitle("It's My Life");
+            song4.setTitle("Lose Yourself");
             song4.setBand("Bon Jovi");
             song4.setGenre("Rock");
             song4.setLinkSpotify("https://open.spotify.com/track/0v1XpBHnsbkCn7iJ9Ucr1l?si=fddfb65ff3c64b1d");
             song4.setLinkYoutube("https://www.youtube.com/watch?v=vx2u5uUu3DE");
             song4.setCode(UUID.randomUUID().toString());
 
-
+            Song song5 = new Song();
+            song5.setTitle("It's My Life");
+            song5.setBand("Bon Jovi");
+            song5.setGenre("Rock");
+            song5.setLinkSpotify("https://open.spotify.com/track/0v1XpBHnsbkCn7iJ9Ucr1l?si=fddfb65ff3c64b1d");
+            song5.setLinkYoutube("https://www.youtube.com/watch?v=vx2u5uUu3DE");
+            song5.setCode(UUID.randomUUID().toString());
 
             songRepository.save(song1);
             songRepository.save(song2);
             songRepository.save(song3);
             songRepository.save(song4);
+
+            Song withoutMe = new Song();
+            withoutMe.setTitle("Without Me");
+            withoutMe.setBand("Eminem");
+            withoutMe.setGenre("Hip hop");
+            withoutMe.setCode(UUID.randomUUID().toString());
+            songRepository.save(withoutMe)
+       
+            Song loseYourself = new Song();
+            loseYourself.setTitle("Lose Yourself");
+            loseYourself.setBand("Eminem");
+            loseYourself.setGenre("Hip hop");
+            loseYourself.setCode(UUID.randomUUID().toString());
+            songRepository.save(loseYourself)
+
+            // Eminem - When I'm Gone
+            Song whenImGone = new Song();
+            whenImGone.setTitle("When I'm Gone");
+            whenImGone.setBand("Eminem");
+            whenImGone.setGenre("Hip hop");
+            whenImGone.setCode(UUID.randomUUID().toString());
+            songRepository.save(whenImGone)
+
+            // Eminem - Not Afraid
+            Song notAfraid = new Song();
+            notAfraid.setTitle("Not Afraid");
+            notAfraid.setBand("Eminem");
+            notAfraid.setGenre("Hip hop");
+            notAfraid.setCode(UUID.randomUUID().toString());
+            songRepository.save(notAfraid)
+            // Eminem - Venom
+            Song venom = new Song();
+            venom.setTitle("Venom");
+            venom.setBand("Eminem");
+            venom.setGenre("Hip hop");
+            venom.setCode(UUID.randomUUID().toString());
+            songRepository.save(venom)
         }
 
 
